e5a7a44637734f911fa732b317898d985b06bd87
Derek McGee <dmcgee2001@gmail.com>
2023-12-21 16:52:03 -0500

    adds pagination

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -2,9 +2,14 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import { GamesIndex } from "./GamesIndex";
 import { GamesNew } from "./GamesNew";
+import { Routes, useNavigate } from "react-router-dom";
+import { Route } from "react-router-dom";
 
 export function Content() {
   const [games, setGames] = useState([]);
+  const [currentPage, setCurrentPage] = useState(0);
+  const itemsPerPage = 12;
+  const navigate = useNavigate();
 
   const handleIndexGames = () => {
     console.log("handleIndexGames");
@@ -21,12 +26,55 @@ export function Content() {
       successCallback();
     });
   };
-  useEffect(handleIndexGames, []);
+
+  useEffect(handleIndexGames, [currentPage]);
+
+  const totalPages = Math.ceil(games.length / itemsPerPage);
+
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+    navigate(`/?page=${page}`);
+  };
+
+  const renderPagination = () => {
+    const maxVisibleButtons = 5;
+    let startPage = Math.max(0, currentPage - Math.floor(maxVisibleButtons / 2));
+    let endPage = Math.min(totalPages - 1, startPage + maxVisibleButtons - 1);
+
+    if (totalPages <= maxVisibleButtons) {
+      startPage = 0;
+      endPage = totalPages - 1;
+    } else if (currentPage <= Math.floor(maxVisibleButtons / 2)) {
+      endPage = maxVisibleButtons - 1;
+    } else if (currentPage >= totalPages - Math.floor(maxVisibleButtons / 2)) {
+      startPage = totalPages - maxVisibleButtons;
+      endPage = totalPages - 1;
+    }
+
+    return (
+      <div>
+        {currentPage > 0 && <button onClick={() => handlePageChange(currentPage - 1)}>Previous</button>}
+        {Array.from({ length: endPage - startPage + 1 }, (_, index) => (
+          <button
+            key={startPage + index}
+            onClick={() => handlePageChange(startPage + index)}
+            disabled={currentPage === startPage + index}
+          >
+            {startPage + index + 1}
+          </button>
+        ))}
+        {currentPage < totalPages - 1 && <button onClick={() => handlePageChange(currentPage + 1)}>Next</button>}
+      </div>
+    );
+  };
 
   return (
-    <div>
-      <GamesIndex games={games} />
-      <GamesNew onCreateGame={handleCreateGame} />
+    <div className="container">
+      <Routes>
+        <Route path="/" element={<GamesIndex games={games} currentPage={currentPage} itemsPerPage={itemsPerPage} />} />
+        <Route path="/games/new" element={<GamesNew onCreateGame={handleCreateGame} />} />
+      </Routes>
+      {renderPagination()}
     </div>
   );
 }
--- src/components/GamesIndex.jsx
+++ src/components/GamesIndex.jsx
@@ -1,12 +1,16 @@
 /* eslint-disable react/prop-types */
-export function GamesIndex(props) {
+export function GamesIndex({ games, currentPage, itemsPerPage }) {
+  const startIndex = currentPage * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const currentGames = games.slice(startIndex, endIndex);
+
   return (
     <div>
       <h1>All games</h1>
-      {props.games.map((game) => (
+      {currentGames.map((game) => (
         <div key={game.id}>
           <h2>{game.name}</h2>
-          <img src={game.background_image} />
+          <img src={game.background_image} alt={game.name} />
           <p>Released: {game.released}</p>
         </div>
       ))}
